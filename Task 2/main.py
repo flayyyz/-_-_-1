0aea64c1a50426561cb57914385c6ce45d5ffaab
flayyyz <106537370+flayyyz@users.noreply.github.com>
2022-06-02 16:26:17 +0300

    Add files via upload

---
+++ Task 2/main.py
@@ -0,0 +1,57 @@
+while True:
+    s = 'yourflagis{23|8|1|20|4|15|25|15|21|11|14|15|23|1|2|15|21|20|1|2|3}'
+    while '26' in s:
+        s = s.replace('26', 'Z', 1)
+    while '25' in s:
+        s = s.replace('25', 'Y', 1)
+    while '24' in s:
+        s = s.replace('24', 'X', 1)
+    while '23' in s:
+        s = s.replace('23', 'W', 1)
+    while '22' in s:
+        s = s.replace('22', 'V', 1)
+    while '21' in s:
+        s = s.replace('21', 'U', 1)
+    while '10' in s:
+        s = s.replace('10', 'J', 1)
+    while '11' in s:
+        s = s.replace('11', 'K', 1)
+    while '12' in s:
+        s = s.replace('12', 'L', 1)
+    while '13' in s:
+        s = s.replace('13', 'M', 1)
+    while '14' in s:
+        s = s.replace('14', 'N', 1)
+    while '15' in s:
+        s = s.replace('15', 'O', 1)
+    while '16' in s:
+        s = s.replace('16', 'P', 1)
+    while '17' in s:
+        s = s.replace('17', 'Q', 1)
+    while '18' in s:
+        s = s.replace('18', 'R', 1)
+    while '19' in s:
+        s = s.replace('19', 'S', 1)
+    while '20' in s:
+        s = s.replace('20', 'T', 1)
+    while '1' in s:
+        s = s.replace('1', 'A', 1)
+    while '2' in s:
+        s = s.replace('2', 'B', 1)
+    while '3' in s:
+        s = s.replace('3', 'C', 1)
+    while '4' in s:
+        s = s.replace('4', 'D', 1)
+    while '5' in s:
+        s = s.replace('5', 'E', 1)
+    while '6' in s:
+        s = s.replace('6', 'F', 1)
+    while '7' in s:
+        s = s.replace('7', 'G', 1)
+    while '8' in s:
+        s = s.replace('8', 'H', 1)
+    while '9' in s:
+        s = s.replace('9', 'I', 1)
+    print(s)  
+    break
+  
